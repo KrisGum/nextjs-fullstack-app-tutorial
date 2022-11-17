3ab2d4e8e17f5ba1630cb5c7bb195fb529ccded2
KrisGum <krisgumularz@gmail.com>
2022-11-17 15:19:13 +0100

    feat: create CatCard

---
+++ components/cards/cat/CatCard.mocks.ts
@@ -0,0 +1,18 @@
+import { ICatCard } from './CatCard';
+
+const base: ICatCard = {
+  tag: 'Felines',
+  title: `What's new in Cats`,
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi perferendis molestiae non nemo doloribus. Doloremque, nihil! At ea atque quidem!',
+  author: 'Alex',
+  time: '2h ago',
+};
+
+//const alt: ICatCard = {
+//  sampleTextProps: 'Hello alt world',
+//};
+
+export const mockCatCardProps = {
+  base,
+  //alt,
+};
